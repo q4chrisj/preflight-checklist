7f0ebb4d6f733921f820a3f65c4ac1536d1fb3fa
Chris Jones <chrisj@q4inc.com>
2023-05-12 16:03:58 -0400

    fix: console logging

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import fs from 'fs';
+import path from 'path';
 
-const token = core.getInput("token");
-const octokit = github.getOctokit(token);
-const repo = github.context.repo;
+// const token = core.getInput("token");
+// const octokit = github.getOctokit(token);
+// const repo = github.context.repo;
 
 async function run(): Promise<void> {
 
+  let workspace: string = process.env.GITHUB_WORKSPACE || "";
+  console.log(workspace);
+  const workspace_files = fs.readdir(workspace, (err, files) => {
+    if (err) {
+      console.error(err);
+    }
+
+    files.forEach(element => {
+      console.log(element);
+    });
+
+
+  });
 }
 
 run()
