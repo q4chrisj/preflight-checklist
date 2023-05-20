67c4040bcbc4a0205b9befdac8603400f2a4533d
Chris Jones <chrisj@q4inc.com>
2023-05-20 09:23:29 -0400

    better for actually demoing

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,43 @@ import path from 'path';
 import { findMatchesInFiles, getAllFiles } from "./files";
 require('aws-sdk/lib/maintenance_mode_message').suppress = true;
 
+/*
+ * For demo:
+ * - Create an object that contains the app_name, path to the correspnding file
+ * - Then loop through each set and call findMatchesInFiles with an array of one file (the one from above)
+ * - Proceed as usual
+ */
 async function run(): Promise<void> {
 
+  // const app_name = "platform-studio-eds"
+  // let workspace: string = process.env.GITHUB_WORKSPACE || path.join(__dirname, "../");
+  // let repo_files: Array<string> = getAllFiles(workspace, []);
+
   const param_search_path = "/dev/"
-  const app_name = "platform-studio-eds"
-  let workspace: string = process.env.GITHUB_WORKSPACE || path.join(__dirname, "../");
-  let repo_files: Array<string> = getAllFiles(workspace, []);
-  let found_parameters: Array<string> = findMatchesInFiles(repo_files, app_name);
+  const Tests: Map<string, string> = new Map();
+  Tests.set("platform-studio-eds", path.join(__dirname, "../test_data/studio-eds-ssm.tf"));
+  Tests.set("platform-shell", path.join(__dirname, "../test_data/platform-shell.tf"));
+  Tests.set("platform-events-eds", path.join(__dirname, "../test_data/events-eds.tf"));
 
-  console.log('\nSSM parameters found in: %s\n', workspace);
-  found_parameters.forEach(f => {
-    console.log("\t - %s", f);
-  })
+  for (let [key, value] of Tests) {
+    let found_parameters: Array<string> = findMatchesInFiles([value], key);
+    console.log('\nSSM parameters found in: %s\n', key);
+    found_parameters.forEach(f => {
+      console.log("\t - %s", f);
+    })
 
-  let aws_parameters = await getAllParameters(param_search_path);
-  let missing = found_parameters.filter(item => aws_parameters.indexOf(item) < 0);
+    let aws_parameters = await getAllParameters(param_search_path);
+    let missing = found_parameters.filter(item => aws_parameters.indexOf(item) < 0);
 
-  console.log("\nThe following SSM parameters don't exist in the target AWS account.\n")
-  missing.forEach(item => {
-    console.warn("\t - %s", item);
-  });
+    console.log("\nThe following SSM parameters don't exist in the target AWS account.\n")
+    missing.forEach(item => {
+      console.warn("\t - %s", item);
+    });
 
-  console.log("");
+    console.log("");
+  }
+
+  return;
 }
 
 const getAllParameters = async (searchPath: string): Promise<Array<string>> => {
